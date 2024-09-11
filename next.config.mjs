fb5aeaf8be3b0ef390548056e03eaf29a2008020
Jakob Haag <Jakob.haag@spicetech.de>
2024-09-11 22:15:15 +0200

    remove unnecessary code

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,4 @@ import { generateSchema } from '@gql.tada/cli-utils';
 
 const nextConfig = {};
 
-const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-
-await generateSchema({
-  input: 'https://graphql.contentful.com/content/v1/spaces/bt6alqgdjs1c/environments/master',
-  output: './schema.graphql',
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
-  tsconfig: undefined,
-  env: {
-    ACCESS_TOKEN: accessToken,
-    SPACE_ID: process.env.SPACE_ID,
-    ENVIRONMENT: process.env.ENVIRONMENT,
-  },
-});
-
 export default nextConfig;
